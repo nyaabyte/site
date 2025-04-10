5c17c85ceb7b31cd5adcf345925aec1c52694d4c
dotle31 <abacubabacus@gmail.com>
2025-04-10 11:23:35 +1000

    i cant spell :sob:

--- src/data/members.ts
+++ src/data/members.ts
@@ -116,7 +116,7 @@ export const sampleMembers: Member[] = [
     bio: "bad girl, silly, stupid and cant write goog code",
     socials: {
       github: "https://github.com/doyle31",
-      website: "https://dotle31.com",
+      website: "https://doyle31.com",
       signal:
         "https://signal.me/#eu/1Z80joyD9Vh7LI1m41BSjvelAf-nyWkq_5FC62P16Mtud86BEn9NB7ceq5ZP09Xw",
     },
