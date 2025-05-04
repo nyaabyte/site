05ff424fcc1ec7baa290fdc514ea2e98a2e3546f
dotle31 <abacubabacus@gmail.com>
2025-05-04 23:42:01 +1000

    Revert "make the code actually unreadable"
    This reverts commit 4bf7f409f4ffc136d4244839ec8b6fd628d92cfc.

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,57 +2,57 @@ import React from "react";
 import styles from "./Contact.module.css";
 
 const Contact: React.FC = () => {
-  const githubLink = "https://github.com/nyaabyte";
-  const emailAddress = "contact@nyabyte.dev";
-  const donateLink = "https://meowguardon.top";
-  return (
-    <section id="contact" className={styles.contact}>
-      <div className={styles.container}>
-        <h2>
-          <span className={styles.highlight}>Contacts</span>
-        </h2>
-        <p className={styles.intro}>Let's yap, contact us here :3</p>
+	const githubLink = "https://github.com/nyaabyte";
+	const emailAddress = "contact@nyabyte.dev";
+	const donateLink = "https://meowguardon.top";
+	return (
+		<section id="contact" className={styles.contact}>
+			<div className={styles.container}>
+				<h2>
+					<span className={styles.highlight}>Contacts</span>
+				</h2>
+				<p className={styles.intro}>Let's yap, contact us here :3</p>
 
-        <div className={styles.contactMethods}>
-          <a
-            href={githubLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`${styles.contactCard} ${styles.github}`}
-          >
-            <div className={styles.iconPlaceholder}>💻</div>
-            <h3>Check our GitHub</h3>
-            <p>Come look at our cool shit!</p>
-            <span className={styles.cta}>View Organization &rarr;</span>
-          </a>
+				<div className={styles.contactMethods}>
+					<a
+						href={githubLink}
+						target="_blank"
+						rel="noopener noreferrer"
+						className={`${styles.contactCard} ${styles.github}`}
+					>
+						<div className={styles.iconPlaceholder}>💻</div>
+						<h3>Check our GitHub</h3>
+						<p>Come look at our cool shit!</p>
+						<span className={styles.cta}>View Organization &rarr;</span>
+					</a>
 
-          <a
-            href={`mailto:${emailAddress}`}
-            className={`${styles.contactCard} ${styles.email}`}
-          >
-            <div className={styles.iconPlaceholder}>✉️</div>
-            <h3>Send us an Email</h3>
-            <p> Contact us quickly!</p>
-            <span className={styles.cta}>{emailAddress} &rarr;</span>
-          </a>
-        </div>
+					<a
+						href={`mailto:${emailAddress}`}
+						className={`${styles.contactCard} ${styles.email}`}
+					>
+						<div className={styles.iconPlaceholder}>✉️</div>
+						<h3>Send us an Email</h3>
+						<p> Contact us quickly!</p>
+						<span className={styles.cta}>{emailAddress} &rarr;</span>
+					</a>
+				</div>
 
-        <br />
+				<br />
 
-        <div className={styles.contactMethods}>
-          <a
-            href={donateLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`${styles.contactCard} ${styles.github}`}
-          >
-            <div className={styles.iconPlaceholder}>💸</div>
-            <h3>Help us make more</h3>
-            <p>Donate and help us pay for bigger projects.</p>
-            <span className={styles.cta}>Open our Kofi &rarr;</span>
-          </a>
+				<div className={styles.contactMethods}>
+					<a
+						href={donateLink}
+						target="_blank"
+						rel="noopener noreferrer"
+						className={`${styles.contactCard} ${styles.github}`}
+					>
+						<div className={styles.iconPlaceholder}>💸</div>
+						<h3>Help us make more</h3>
+						<p>Donate and help us pay for bigger projects.</p>
+						<span className={styles.cta}>Open our Kofi &rarr;</span>
+					</a>
 
-          {/* <a
+					{/* <a
 						href={`mailto:${emailAddress}`}
 						className={`${styles.contactCard} ${styles.email}`}
 					>
@@ -61,16 +61,16 @@ const Contact: React.FC = () => {
 						<p> Contact us quickly!</p>
 						<span className={styles.cta}>{emailAddress} &rarr;</span>
 					</a> */}
-        </div>
-      </div>
+				</div>
+			</div>
 
-      <div className={styles.backgroundShapes}>
-        <div className={`${styles.shape} ${styles.shape1}`}></div>
-        <div className={`${styles.shape} ${styles.shape2}`}></div>
-        <div className={`${styles.shape} ${styles.shape3}`}></div>
-      </div>
-    </section>
-  );
+			<div className={styles.backgroundShapes}>
+				<div className={`${styles.shape} ${styles.shape1}`}></div>
+				<div className={`${styles.shape} ${styles.shape2}`}></div>
+				<div className={`${styles.shape} ${styles.shape3}`}></div>
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,88 +4,88 @@ import logoSrc from "/src/assets/logo.png";
 import { Link } from "react-scroll";
 
 const Header = () => {
-  const [scrolled, setScrolled] = useState(true);
+	const [scrolled, setScrolled] = useState(true);
 
-  const handleScroll = () => {
-    if (window.scrollY > 0) {
-      setScrolled(false);
-    } else {
-      setScrolled(true);
-    }
-  };
+	const handleScroll = () => {
+		if (window.scrollY > 0) {
+			setScrolled(false);
+		} else {
+			setScrolled(true);
+		}
+	};
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  // handleScroll();
+	// handleScroll();
 
-  return (
-    <header
-      // className={styles.header}
-      className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}
-    >
-      <div className={styles.container}>
-        <Link to="top" smooth={true} duration={500} className={styles.link}>
-          <div className={styles.logo}>
-            <img src={logoSrc} alt="NyaByte Logo" />
-          </div>
-        </Link>
-        <nav className={styles.nav}>
-          <ul>
-            <li>
-              <Link
-                to="about"
-                smooth={true}
-                duration={500}
-                offset={-60}
-                className={styles.link}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="projects"
-                smooth={true}
-                duration={500}
-                offset={-60}
-                className={styles.link}
-              >
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="members"
-                smooth={true}
-                duration={500}
-                offset={-60}
-                className={styles.link}
-              >
-                Members
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="contact"
-                smooth={true}
-                duration={500}
-                offset={-60}
-                className={styles.link}
-              >
-                Contacts
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+	return (
+		<header
+			// className={styles.header}
+			className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}
+		>
+			<div className={styles.container}>
+				<Link to="top" smooth={true} duration={500} className={styles.link}>
+					<div className={styles.logo}>
+						<img src={logoSrc} alt="NyaByte Logo" />
+					</div>
+				</Link>
+				<nav className={styles.nav}>
+					<ul>
+						<li>
+							<Link
+								to="about"
+								smooth={true}
+								duration={500}
+								offset={-60}
+								className={styles.link}
+							>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="projects"
+								smooth={true}
+								duration={500}
+								offset={-60}
+								className={styles.link}
+							>
+								Projects
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="members"
+								smooth={true}
+								duration={500}
+								offset={-60}
+								className={styles.link}
+							>
+								Members
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="contact"
+								smooth={true}
+								duration={500}
+								offset={-60}
+								className={styles.link}
+							>
+								Contacts
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
