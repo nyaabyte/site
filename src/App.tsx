462e3e6e1ac30d7ca6c8dbec8d7c1e5534404882
dotle31 <abacubabacus@gmail.com>
2025-05-05 09:56:32 +1000

    Revert "fix broken commas in bun lock file, plus indentation"
    This reverts commit fd91fbe8a267ace914f8bad03cc61059fd5c9157.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import styles from "./App.module.css";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact";
-import Members from "./components/Members/Members";
+import styles from './App.module.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Members from './components/Members/Members';
 
 const App = () => {
-  return (
-    <div className={styles.appContainer}>
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Projects />
-        <Members />
-        <Contact />
-      </main>
-    </div>
-  );
+	return (
+		<div className={styles.appContainer}>
+			<Header />
+			<main>
+				<Hero />
+				<About />
+				<Projects />
+				<Members />
+				<Contact />
+			</main>
+		</div>
+	);
 };
 
 export default App;
