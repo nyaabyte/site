178d89c1cc3ed8115a8feeb37f5a8ae9a7400a75
dotle31 <abacubabacus@gmail.com>
2025-05-04 23:23:09 +1000

    make the code readable

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,18 @@ import Contact from './components/Contact/Contact';
 import Members from './components/Members/Members';
 
 const App = () => {
-  return (
-    <div className={styles.appContainer}>
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Projects />
-        <Members />
-        <Contact />
-      </main>
-    </div>
-  );
+	return (
+		<div className={styles.appContainer}>
+			<Header />
+			<main>
+				<Hero />
+				<About />
+				<Projects />
+				<Members />
+				<Contact />
+			</main>
+		</div>
+	);
 };
 
 export default App;
